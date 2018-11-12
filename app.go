6b19ae315ab635a4f3f5ba0529b061be21b9dd58
Philip Papurt <philip@emvoiceapp.com>
2018-11-11 22:59:51 -0500

    add security headers

--- app.go
+++ app.go
@@ -1,83 +1,101 @@
 package main
 
 import (
-  "os"
-  "sync"
-  "time"
-  "net/http"
-  "io/ioutil"
-  "encoding/json"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"sync"
+	"time"
 )
 
 type redir struct {
-  To string `json:"to"`
+	To string `json:"to"`
 }
 
 type redirList struct {
-  Mux    sync.RWMutex
-  Redirs map[string]redir
+	mux    sync.RWMutex
+	redirs map[string]redir
 }
 
-var appRedirList redirList
-
-func fetchRedirs() error {
-  res, getErr := http.Get("https://api.github.com/gists/" + os.Getenv("APP_GIST_ID"))
-  if getErr != nil {
-    return getErr
-  }
-  defer res.Body.Close()
-  content, readErr := ioutil.ReadAll(res.Body)
-  if readErr != nil {
-    return readErr
-  }
-  var contentJSON interface{}
-  contentJSONErr := json.Unmarshal(content, &contentJSON)
-  if contentJSONErr != nil {
-    return contentJSONErr
-  }
-  fileContent := contentJSON.(map[string]interface{})["files"].(map[string]interface{})["link"].(map[string]interface{})["content"].(string)
-  appRedirList.Mux.Lock()
-  fileJSONErr := json.Unmarshal([]byte(fileContent), &appRedirList.Redirs)
-  if fileJSONErr != nil {
-    return fileJSONErr
-  }
-  appRedirList.Mux.Unlock()
-  return nil
+type server struct {
+	redirList redirList
 }
 
-func startRedirFetch() {
-  ticker := time.NewTicker(10 * time.Second)
-  quit := make(chan struct{})
-  go func() {
-    for {
-      select {
-      case <-ticker.C:
-        fetchRedirs()
-      case <-quit:
-        ticker.Stop()
-        return
-      }
-    }
-  }()
+func (s *server) fetchRedirs() error {
+	res, getErr := http.Get(fmt.Sprintf(
+		"https://api.github.com/gists/%s?client_id=%s&client_secret=%s",
+		os.Getenv("APP_GITHUB_GIST_ID"),
+		os.Getenv("APP_GITHUB_CLIENT_ID"),
+		os.Getenv("APP_GITHUB_CLIENT_SECRET"),
+	))
+	if getErr != nil {
+		return getErr
+	}
+	defer res.Body.Close()
+	content, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		return readErr
+	}
+	var contentJSON map[string]interface{}
+	contentJSONErr := json.Unmarshal(content, &contentJSON)
+	if contentJSONErr != nil {
+		return contentJSONErr
+	}
+	fileContent := contentJSON["files"].(map[string]interface{})["link"].(map[string]interface{})["content"].(string)
+	s.redirList.mux.Lock()
+	fileJSONErr := json.Unmarshal([]byte(fileContent), &s.redirList.redirs)
+	if fileJSONErr != nil {
+		return fileJSONErr
+	}
+	s.redirList.mux.Unlock()
+	return nil
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-  appRedirList.Mux.RLock()
-  selectedRedir, ok := appRedirList.Redirs[r.URL.Path]
-  appRedirList.Mux.RUnlock()
-  if !ok {
-    w.Write([]byte("404"))
-    return
-  }
-  http.Redirect(w, r, selectedRedir.To, 302)
+func (s *server) startRedirFetch() {
+	ticker := time.NewTicker(10 * time.Second)
+	quit := make(chan struct{})
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				s.fetchRedirs()
+			case <-quit:
+				ticker.Stop()
+				return
+			}
+		}
+	}()
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	headers := w.Header()
+	headers.Set("strict-transport-security", "max-age=31536000; includeSubDomains; preload")
+	headers.Set("content-security-policy", "default-src 'none'; sandbox")
+	headers.Set("referrer-policy", "no-referrer")
+	headers.Set("x-content-type-options", "nosniff")
+	headers.Set("x-frame-options", "SAMEORIGIN")
+	headers.Set("x-xss-protection", "1; mode=block")
+	requestPath := path.Clean(r.URL.EscapedPath())
+	s.redirList.mux.RLock()
+	selectedRedir, ok := s.redirList.redirs[requestPath]
+	s.redirList.mux.RUnlock()
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("404"))
+		return
+	}
+	http.Redirect(w, r, selectedRedir.To, http.StatusFound)
 }
 
 func main() {
-  fetchErr := fetchRedirs()
-  if fetchErr != nil {
-    panic(fetchErr)
-  }
-  startRedirFetch()
-  http.HandleFunc("/", handleRequest)
-  panic(http.ListenAndServe(":80", nil))
+	s := server{}
+	fetchErr := s.fetchRedirs()
+	if fetchErr != nil {
+		panic(fetchErr)
+	}
+	s.startRedirFetch()
+	panic(http.ListenAndServe(":80", &s))
 }
